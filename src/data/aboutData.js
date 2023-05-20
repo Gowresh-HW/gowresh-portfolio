1053d67314154897d25bc1976002a910d72f8506
Gowresh Rajagopal <114281989+Gowresh-HW@users.noreply.github.com>
2023-05-20 19:01:32 +0400

    Delete src directory

--- src/data/aboutData.js
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-    title: "Who I am",
-    description1: "Hello! My name is Gowresh Rajagopal and I am a Roboticist with a passion (more like an obsession!) for Robotics.",
-    description2: "I am currently working as a Full-Stack ROS Developer/Robotics Engineer at Maxbyte Technologies, UAE. I started my career as a Junior Robotics Engineer at Maxbyte Technologies, India. Over the past 5 years I have evolved as a full-stack ROS Developer across multiple mobile robot platforms, specifically focusing on development and deployment of Autonomous Industrial Mobility Vehicles to shopfloor environments. I am also currently pursuing my Masters in Robotics at Heriot-Watt University, Dubai, where I got the opportunity to work on self-driving vehicle as part of my thesis. Other than this I consider myself a bookworm, who not just loves reading books but also likes to buy and maintain a personal library. And if it is not not obvious yet, I am an ardent Potterhead!",
-    image: 2
-}
--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'gowresh7@gmail.com',
-    phone: '+91 8015741729, +971 524659664',
-    address: 'Discovery Gardens, Dubai, UAE',
-
-    sheetAPI: ''
-}
